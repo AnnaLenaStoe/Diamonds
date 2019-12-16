ca4e4c8417c0b85e02e4cd46a07456ea14fb74cd
AnnaLenaStoe <AnnSto16@student.fh-westkueste.de>
2019-12-16 13:14:42 +0100

    Soweit Programm eingearbeitet, Probleme mit Zeile 58

--- Diamonds.py
+++ Diamonds.py
@@ -2,34 +2,73 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-#%matplotlib inline
+%matplotlib inline
+import math
 
-#sns.set(context="notebook", palette="Spectral", style = 'darkgrid' ,font_scale = 1.5, color_codes=True)
-#import warnings
-#warnings.filterwarnings('ignore')
-#%matplotlib inline
-#plt.style.use('ggplot')
-#ggplot is R based visualisation package that provides better graphics with higher level of abstraction
-#import os
 diamond = pd.read_csv('diamonds.csv')
-# Dataset importieren, die csv Datei von Kaggle
-print (diamond)
-# Die Liste wird im Fenster angezeigt
-diamond.info()
-print (diamond.info())
-# Sie besteht aus 53940 Zeilen und 11 Spalten
-# Daten zu Karat, Schnitt, Farbe, Preis, Reinheit, Tiefe, x, y, z
-# Karat ist das Gewicht des Diamanten bzw. eines Minerals/Edelsteins
-# Schnitt hat eine Skala von Fair, Good, Very Good, Premium, Ideal
-# Farbe von D (am besten) bis J (am schlechtesten)
-# Preis
-#Spalte "Unnamed" brauchen wir nicht, also entfernen:
 
-diamond = diamond.drop (["Unnamed: 0"], axis=1)
 diamond.head()
-print (diamond)
 
-#Heatmap
-#plt.figure(figsize=(20,20))
-#p=sns.heatmap(diamond.corr(), annot=True,cmap='RdYlGn',square=True)
-#sns.pairplot(diamond)
+diamond.info()
+
+diamond.describe()
+
+diamond.columns
+diamond.drop(['Unnamed: 0'] , axis=1 , inplace=True)
+diamond.describe ()
+
+sns.pairplot(diamond)
+
+sns.distplot(diamond['price'])
+
+sns.heatmap(diamond.corr())
+
+#sns.factorplot(data=diamond , kind='box' , size=7, aspect=2.5)
+
+#sns.kdeplot(diamond['carat'], shade=True , color='b')
+
+#sns.jointplot(x='carat' , y='price' , data=diamond , size=5)
+
+#sns.factorplot(x='cut', data=diamond , kind='count',aspect=2.5 )
+
+#sns.factorplot(x='cut', y='price', data=diamond, kind='box' ,aspect=2.5 )
+
+#sns.factorplot(x='color', data=diamond , kind='count',aspect=2.5 )
+
+#sns.factorplot(x='color', y='price' , data=diamond , kind='violin', aspect=2.5)
+
+#sns.boxplot(x='clarity', y='price', data=diamond)
+
+#plt.hist('depth' , data=diamond , bins=25)
+
+#sns.jointplot(x='depth', y='price' , data=diamond , kind='regplot', size=5)
+
+#sns.kdeplot(diamond['table'] ,shade=True , color='b')
+
+x = diamond[['carat', 'cut', 'color', 'clarity', 'depth', 'table', 'x', 'y', 'z']]
+y = diamond['price']
+
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.4, random_state=101)
+
+from sklearn.linear_model import LinearRegression
+
+lr = LinearRegression()
+
+lr.fit(x_train,y_train)
+
+coeff_diamond = pd.DataFrame(lr.coef_,x.columns,columns=['Coefficient'])
+coeff_diamond
+
+predictions = lr.predict(x_test)
+
+plt.scatter(y_test,predictions)
+
+sns.distplot((y_test-predictions),bins=50);
+
+from sklearn import metrics
+print('MAE:', metrics.mean_absolute_error(y_test, predictions))
+print('MSE:', metrics.mean_squared_error(y_test, predictions))
+print('RMSE:', np.sqrt(metrics.mean_squared_error(y_test, predictions)))
+
